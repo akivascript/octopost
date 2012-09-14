95b596dc39528fd4ab02fe3dbf8be417f71205a6
Akiva <akiva@misanthropicgeek.com>
2012-09-14 07:22:31 -0700

    Remove broken logging.

--- octopost.sh
+++ octopost.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-LOG="$(/usr/bin/ruby -X /usr/local/bin octopost.rb $1 $2 $3)\n"
+/usr/bin/ruby -X /usr/local/bin octopost.rb $1 $2 $3
 cd $1
-LOG="$LOG $(rake generate 2>&1)"
-LOG="$LOG $(git pull 2>&1)"
-LOG="$LOG $(git add -A 2>&1)"
-LOG="$LOG $(git commit -m 'Add latest post.' 2>&1)"
-LOG="$LOG $(git push 2>&1)"
-echo $LOG
+rake generate
+git pull
+git add -A
+git commit -m 'Add latest entry.'
+git push
