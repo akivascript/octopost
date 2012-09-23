e66465fc6382656d0537319323ea25fef6e2af09
Akiva <akiva@misanthropicgeek.com>
2012-09-23 09:10:15 -0700

    Add separator gsub.

--- octopost.rb
+++ octopost.rb
@@ -7,6 +7,7 @@ filename 	= CGI.unescape(ARGV[1])
 filename	= filename.to_url
 post 		= CGI.unescape(ARGV[2])
 post.gsub!("\r", "\n")
+post.gsub!("• • • • •", "<p class=\"separator\">• • • • •</p>")
 
 filehandle = File.new(path + "/" + filename + ".markdown", "w")
 filehandle.write(post)
