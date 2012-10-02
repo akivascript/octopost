2f8ee1dd41a728d17e11a4f66799ad0d633c66f8
Akiva <git@misanthropicgeek.com>
2012-10-02 09:54:25 -0700

    Add logging.

--- octopost.sh
+++ octopost.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 /usr/bin/ruby -X /usr/local/bin octopost.rb $1 $2 $3
 cd $1
-rake generate
-git pull
-git add -A
-git commit -m 'Add latest entry.'
-git push
+
+log="Pulling latest... $(git pull 2>&1)"
+log="$log Generating site... $(rake generate 2>&1)"
+log="$log Adding site bits... $(git add -A 2>&1)"
+log="$log $(git commit -m 'Add latest entry.' 2>&1)"
+log="$log Publishing site... $(git push 2>&1)"
+
+echo $log
